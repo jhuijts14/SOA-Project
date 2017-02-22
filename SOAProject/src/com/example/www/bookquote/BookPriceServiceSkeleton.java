614a3d4463497ff9e3f0bc4a1aceeb5ce7dae791
jhuijts14 <joephuijts14@gmail.com>
2017-02-22 09:42:35 +0100

    Made an XSD and WSDL file
    Did ex. 5.5

--- SOAProject/src/com/example/www/bookquote/BookPriceServiceSkeleton.java
+++ SOAProject/src/com/example/www/bookquote/BookPriceServiceSkeleton.java
@@ -3,30 +3,42 @@
  * BookPriceServiceSkeleton.java
  *
  * This file was auto-generated from WSDL
- * by the Apache Axis2 version: 1.6.4  Built on : Dec 28, 2015 (10:03:39 GMT)
+ * by the Apache Axis2 version: 1.5.1  Built on : Oct 19, 2009 (10:59:00 EDT)
  */
-    package com.example.www.bookquote;
+package com.example.www.bookquote;
+
+import java.util.*;
     /**
      *  BookPriceServiceSkeleton java skeleton for the axisService
      */
-    public class BookPriceServiceSkeleton implements BookPriceServiceSkeletonInterface{
+    public class BookPriceServiceSkeleton implements BookPriceServiceSkeletonInterface {
         
-         
+    	private Map<String,Double> books;
+    	
+        public BookPriceServiceSkeleton () {
+        	books = new HashMap<String,Double>();
+        	books.put("1", 10.0);
+        	books.put("2", 45.0);
+        	books.put("3", 20.0);
+        	books.put("4", 35.0);
+        	books.put("5", 50.0);
+        }
         /**
          * Auto generated method signature
          * 
-                                     * @param getBookPrice0 
-             * @return getBookPriceResponse1 
+                                     * @param getBookPrice
          */
         
                  public com.example.www.bookquote.GetBookPriceResponse getBookPrice
                   (
-                  com.example.www.bookquote.GetBookPrice getBookPrice0
+                  com.example.www.bookquote.GetBookPrice getBookPrice
                   )
             {
                 //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getBookPrice");
-        }
+                	 GetBookPriceResponse response = new GetBookPriceResponse();
+                	 response.setPrice(books.get(getBookPrice.getIsbn()));
+                	 return response;
+            }
      
     }
     
