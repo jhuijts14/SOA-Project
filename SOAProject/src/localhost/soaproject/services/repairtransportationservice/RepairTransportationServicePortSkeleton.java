72491e1ba48158e5b4dc62c96d15835c66cac6de
dcsimmo2 <dcsimmo2@gmail.com>
2017-03-13 21:38:21 +0100

    Distnace and Time operations working

--- SOAProject/src/localhost/soaproject/services/repairtransportationservice/RepairTransportationServicePortSkeleton.java
+++ SOAProject/src/localhost/soaproject/services/repairtransportationservice/RepairTransportationServicePortSkeleton.java
@@ -8,9 +8,15 @@
     package localhost.soaproject.services.repairtransportationservice;
     
     import java.math.BigDecimal;
-    import java.util.Scanner;
+import java.math.RoundingMode;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+import java.util.Calendar;
+import java.util.Date;
 
-    import localhost.soaproject.services.commontypes.*;
+import localhost.soaproject.services.commontypes.*;
     /**
      *  RepairTransportationServicePortSkeleton java skeleton for the axisService
      */
@@ -47,22 +53,14 @@
                   )
             {
                 //TODO : fill this with the necessary business logic
-                	 // Retrieve repair ID from request mmessage;
+                	 // Retrieve repair ID and Address (houseNumber for simplicity) from request message;
                 RepairIDType repairID = getTransportationDistance2.getGetTransportationDistanceRequest().getRepair();
                 int houseNumber = getTransportationDistance2.getGetTransportationDistanceRequest().getAddress().getHouseNumber();
                 
                 //Define Local Variable;
-                String address;
-                BigDecimal bd = new BigDecimal(1.0);
+                //String address;
+                double distance = 0;
                 TransportationDistanceType  transportationDistanceType = new TransportationDistanceType();
-                
-                //Scanner scanner = new Scanner(System.in);
-                
-                //User Scanner to get Customers Address;
-                //System.out.print("What is the address where the bike will be picked up? ");
-                //address = scanner.nextLine();
-                // Simulate the distances depending on the address input
-               // TransportationDistanceType[] transportationDistance = new TransportationDistanceType [1];
                 
                 //OPTIONS FOR INPUT ARE:
                 //Getfertweg 5 7512BA
@@ -70,19 +68,25 @@
                 //Marie de Roodelaan 42 7545RT
                 
                 if (houseNumber == 5) {
-                	bd = 6.2;
+                	distance = 6.2;
                 } else if (houseNumber == 41){
-                	bd = 7.1;
+                	distance = 7.1;
                 } else if (houseNumber == 42){
-                	bd = 4.5;
+                	distance = 4.5;
                 }
                 // Set TransportationDistance
-            	transportationDistanceType.setTransportationDistanceType(distance);                
+            	transportationDistanceType.setTransportationDistanceType((new BigDecimal(distance).setScale(2, RoundingMode.HALF_EVEN)));                
                 
                 //Create the distance response type
                 DistanceResponseType distanceResponseType = new DistanceResponseType();
                 distanceResponseType.setRepair(repairID);
                 distanceResponseType.setTransportationDistance(transportationDistanceType);
+                
+                //Create and return the distance response message;
+                GetTransportationDistanceResponse transportationDistanceResponse = new GetTransportationDistanceResponse();
+                transportationDistanceResponse.setGetTransportationDistanceResponse(distanceResponseType);
+                
+                return transportationDistanceResponse;
         }
      
          
@@ -99,7 +103,61 @@
                   )
             {
                 //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getTransportationTime");
+                // Retrieve repair ID and Address (houseNumber for simplicity) from request message;
+                RepairIDType repairID = getTransportationTime4.getGetTransportationTimeRequest().getRepair();
+                int houseNumber = getTransportationTime4.getGetTransportationTimeRequest().getAddress().getHouseNumber();
+                
+                
+                String pattern = "yyyy-MM-dd HH:mm:ss";
+                DateFormat dateFormat = new SimpleDateFormat(pattern);
+                Calendar cal = Calendar.getInstance();
+                
+                //Local Variables
+                int MinutesAway = 0;
+                
+                TransportationTimeType transportationTimeType = new TransportationTimeType();
+                
+                //OPTIONS FOR INPUT ARE:
+                //Getfertweg 5 7512BA
+                //Hofstraat 41 7511HD
+                //Marie de Roodelaan 42 7545RT
+                
+                if (houseNumber == 5) {
+                	MinutesAway = 15;
+                } else if (houseNumber == 41){
+                	MinutesAway = 12;
+                } else if (houseNumber == 42){
+                	MinutesAway = 10;
+                }
+                
+                cal.add(Calendar.MINUTE, MinutesAway);
+                String newTime = dateFormat.format(cal.getTime());
+                
+                
+                
+                Date date = null;
+				try {
+					date = new SimpleDateFormat(pattern).parse(newTime);
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+                cal.setTime(date);
+                
+                
+                transportationTimeType.setTransportationTimeType(cal);
+                
+                TimeResponseType timeResponseType = new TimeResponseType();
+                timeResponseType.setRepair(repairID);
+                timeResponseType.setTransportationTime(transportationTimeType);
+                
+              //Create and return the distance response message;
+                GetTransportationTimeResponse transportationTimeResponse = new GetTransportationTimeResponse();
+                transportationTimeResponse.setGetTransportationTimeResponse(timeResponseType);
+                
+                return transportationTimeResponse;
+                
+                
         }
      
     }
